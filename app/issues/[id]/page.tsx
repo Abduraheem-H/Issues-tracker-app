e1340fb5a4dc64d4dcf154d0f91a57608aa1dca0
Abduraheem Hussein <abdurahim.muzemil@aait.edu.et>
2025-01-14 12:00:00 +0300

    feat(ui): Add loading components for issue detail and new issue pages

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+const IssueDetailPage = async ({ params }: Props) => {
+  const { id } = await params;
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!issue) notFound();
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">{issue?.title}</h1>
+      <p className="mb-2">
+        <strong>Status:</strong> {issue?.status}
+      </p>
+      <p className="mb-2">
+        <strong>Created At:</strong>{" "}
+        {new Date(issue!.createdAt).toLocaleDateString()}
+      </p>
+      <p>
+        <strong>Description:</strong>
+      </p>
+      <p>{issue?.description}</p>
+    </div>
+  );
+};
+
+export default IssueDetailPage;
