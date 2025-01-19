a45e723b3a0b6043e00aabc18a90b73a08f2fb52
Abduraheem Hussein <abdurahim.muzemil@aait.edu.et>
2025-01-19 12:00:00 +0300

    feat(ui): add edit button to IssueDetailPage and integrate Radix UI icons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,8 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import ReactMarkDown from "react-markdown";
 
@@ -18,20 +20,31 @@ const IssueDetailPage = async ({ params }: Props) => {
   // await new Promise((r) => setTimeout(r, 2000)); // Simulate delay for loading state
 
   return (
-    <div>
-      <Heading className="text-2xl font-bold mb-8">{issue?.title}</Heading>
-      <Flex direction="row" align="center" gap="2" className="mb-4 mt-4">
-        <Text className="font-medium">Status:</Text>
-        <IssueStatusBadge status={issue!.status} />
-        <Text className="font-medium">Created:</Text>
-        <Text className="mb-2">
-          {new Date(issue!.createdAt).toDateString()}
-        </Text>
-      </Flex>
-      <Card className="mt-4 prose">
-        <ReactMarkDown>{issue?.description}</ReactMarkDown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2fr 2fr" }} gap="8">
+      <Box>
+        <Heading className="text-2xl font-bold mb-8">{issue?.title}</Heading>
+        <Flex direction="row" align="center" gap="2" className="mb-4 mt-4">
+          <Text className="font-medium">Status:</Text>
+          <IssueStatusBadge status={issue!.status} />
+          <Text className="font-medium">Created:</Text>
+          <Text className="mb-2">
+            {new Date(issue!.createdAt).toDateString()}
+          </Text>
+        </Flex>
+
+        <Card className="prose" mt="4">
+          <ReactMarkDown>{issue?.description}</ReactMarkDown>
+        </Card>
+      </Box>
+      <Box>
+        <Link href={`/issues/${issue.id}/edit`}>
+          <Button>
+            <Pencil2Icon />
+            Edit Issue
+          </Button>
+        </Link>
+      </Box>
+    </Grid>
   );
 };
 
