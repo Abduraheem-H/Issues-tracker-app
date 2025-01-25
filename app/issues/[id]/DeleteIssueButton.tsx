ac117b859ca1aa2ae030d5bd090ed4df041116f3
Abduraheem Hussein <abdurahim.muzemil@aait.edu.et>
2025-01-25 12:00:00 +0300

    feat(ui): enhance DeleteIssueButton with confirmation dialog for issue deletion

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,29 @@
-import { Button } from "@radix-ui/themes";
-import React from "react";
-
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 const DeleteIssueButton = () => {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>
+          Are you sure you want to delete this issue?
+        </AlertDialog.Title>
+        <AlertDialog.Description>
+          This action cannot be undone.
+        </AlertDialog.Description>
+        <Flex justify="center" gap="2" className="mt-4">
+          <AlertDialog.Action>
+            <Button color="red">Yes, Delete</Button>
+          </AlertDialog.Action>
+          <AlertDialog.Cancel>
+            <Button>Cancel</Button>
+          </AlertDialog.Cancel>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteIssueButton;
