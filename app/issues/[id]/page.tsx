06a7322813af9d1ff8b13eefdbce44677689acb4
Abduraheem Hussein <abdurahim.muzemil@aait.edu.et>
2025-02-23 12:00:00 +0300

    feat(env): add example environment variables and update .gitignore to include .env files

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,17 +7,22 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOption";
 import AssigneeSelect from "./AssigneeSelect";
+import { cache } from "react";
 
 interface Props {
   params: Promise<{ id: string }>;
 }
 
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({
+    where: { id: issueId },
+  })
+);
+
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
   const { id } = await params;
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(id) },
-  });
+  const issue = await fetchIssue(parseInt(id));
 
   if (!issue) notFound();
   // await new Promise((r) => setTimeout(r, 2000)); // Simulate delay for loading state
@@ -44,10 +49,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 
 export async function generateMetadata({ params }: Props) {
   const { id } = await params;
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(id) },
-    select: { title: true, description: true },
-  });
+  const issue = await fetchIssue(parseInt(id));
   return {
     title: issue ? `${issue.title}` : "Issue Not Found",
     description: issue
